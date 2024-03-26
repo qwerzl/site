b01ea54bd2a62e6d9df5a9b54b091f667fb63e98
qwerzl <me@qwerzl.me>
2024-03-26 22:44:06 +0800

    fix: table style

--- components/content/ProseTh.vue
+++ components/content/ProseTh.vue
@@ -1,5 +1,5 @@
 <template>
-  <th class="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
+  <th class="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right bg-muted">
     <slot />
   </th>
 </template>
--- components/content/ProseTr.vue
+++ components/content/ProseTr.vue
@@ -1,5 +1,5 @@
 <template>
-  <tr class="m-0 border-t p-0 even:bg-muted">
+  <tr class="m-0 border-t p-0">
     <slot />
   </tr>
 </template>
